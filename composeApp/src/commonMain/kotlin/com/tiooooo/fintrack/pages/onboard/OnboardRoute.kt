7b6599533a9faecc7d8daf2ea3aa5d6f1404bbcb
Tio Fani <43690617+tiofani03@users.noreply.github.com>
2025-03-28 15:36:52 +0700

    feat: wallet page (#10)

--- composeApp/src/commonMain/kotlin/com/tiooooo/fintrack/pages/onboard/OnboardRoute.kt
+++ composeApp/src/commonMain/kotlin/com/tiooooo/fintrack/pages/onboard/OnboardRoute.kt
@@ -1,140 +1,18 @@
 package com.tiooooo.fintrack.pages.onboard
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
+import cafe.adriel.voyager.core.model.rememberScreenModel
 import cafe.adriel.voyager.core.screen.Screen
-import com.tiooooo.fintrack.component.base.BaseScaffold
-import com.tiooooo.fintrack.component.theme.EXTRA_LARGE_PADDING
-import com.tiooooo.fintrack.component.theme.MEDIUM_PADDING
-import com.tiooooo.fintrack.component.theme.SMALL_PADDING
-import com.tiooooo.fintrack.component.theme.primaryDark
-import com.tiooooo.fintrack.component.theme.primaryLight
-import com.tiooooo.fintrack.component.theme.textMedium14
-import com.tiooooo.fintrack.component.theme.textMedium20
-import com.tiooooo.fintrack.pages.onboard.components.OnBoardStories
-import com.tiooooo.fintrack.pages.onboard.components.OnboardBottomSheet
-import fintrack.composeapp.generated.resources.Res
-import fintrack.composeapp.generated.resources.ic_onboard_data
-import fintrack.composeapp.generated.resources.ic_onboard_happy
-import fintrack.composeapp.generated.resources.ic_onboard_transfer
-import org.jetbrains.compose.resources.painterResource
 
 object OnboardRoute : Screen {
     @Composable
     override fun Content() {
-        val listOfImages = listOf(
-            Res.drawable.ic_onboard_transfer,
-            Res.drawable.ic_onboard_data,
-            Res.drawable.ic_onboard_happy
+        val screenModel = rememberScreenModel { OnboardScreenModel() }
+        OnboardScreen(
+            modifier = Modifier.fillMaxSize(),
+            onboardScreenModel = screenModel,
         )
-
-        val listOfText = listOf(
-            "Ini teks pertama",
-            "Ini teks kedua",
-            "Ini teks ketiga",
-        )
-
-
-        BaseScaffold { paddingValues ->
-            Box(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(bottom = paddingValues.calculateBottomPadding())
-                    .background(Brush.verticalGradient(listOf(primaryDark, primaryLight)))
-            ) {
-                OnBoardStories(
-                    modifier = Modifier
-                        .fillMaxHeight(0.75f)
-                        .padding(top = paddingValues.calculateTopPadding())
-                        .fillMaxWidth()
-                        .padding(bottom = MEDIUM_PADDING),
-                    numberOfPages = listOfImages.size,
-                    onEveryStoryChange = { position ->
-
-                    },
-                    onComplete = {
-
-                    },
-                ) { index ->
-                    Box(modifier = Modifier.fillMaxSize()) {
-                        Image(
-                            modifier = Modifier
-                                .fillMaxHeight(0.55f)
-                                .align(Alignment.Center)
-                                .padding(horizontal = EXTRA_LARGE_PADDING)
-                                .padding(bottom = MEDIUM_PADDING),
-                            painter = painterResource(listOfImages[index]),
-                            contentDescription = null,
-                        )
-                        Column(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .align(Alignment.BottomCenter)
-                                .padding(bottom = MEDIUM_PADDING)
-                        ) {
-                            Text(
-                                modifier = Modifier
-                                    .align(Alignment.CenterHorizontally)
-                                    .padding(horizontal = MEDIUM_PADDING),
-                                text = listOfText[index],
-                                style = textMedium20().copy(
-                                    color = Color.White,
-                                    fontWeight = FontWeight.Bold,
-                                    textAlign = TextAlign.Center,
-                                )
-                            )
-                            Text(
-                                modifier = Modifier
-                                    .align(Alignment.CenterHorizontally)
-                                    .padding(
-                                        top = SMALL_PADDING,
-                                        start = MEDIUM_PADDING,
-                                        end = MEDIUM_PADDING,
-                                    ),
-                                text = "Pake FinTrack, keuanganmu bakal lebih terkontrol tanpa drama.",
-                                style = textMedium14().copy(
-                                    color = Color.White,
-                                    fontWeight = FontWeight.Light,
-                                    textAlign = TextAlign.Center,
-                                )
-                            )
-                        }
-                    }
-                }
-                OnboardBottomSheet(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .fillMaxHeight(0.25f)
-                        .align(Alignment.BottomCenter)
-                        .background(
-                            shape = RoundedCornerShape(
-                                topStart = EXTRA_LARGE_PADDING,
-                                topEnd = EXTRA_LARGE_PADDING,
-                            ),
-                            color = MaterialTheme.colorScheme.background,
-                        ),
-                )
-            }
-        }
     }
 }
---
+++ composeApp/src/commonMain/kotlin/com/tiooooo/fintrack/pages/wallet/components/WalletCardItem.kt
@@ -0,0 +1,150 @@
+package com.tiooooo.fintrack.pages.wallet.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Card
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import com.tiooooo.fintrack.component.theme.EXTRA_LARGE_PADDING
+import com.tiooooo.fintrack.component.theme.EXTRA_SMALL_PADDING
+import com.tiooooo.fintrack.component.theme.HUGE_PADDING
+import com.tiooooo.fintrack.component.theme.LARGE_PADDING
+import com.tiooooo.fintrack.component.theme.MEDIUM_PADDING
+import com.tiooooo.fintrack.component.theme.SMALL_PADDING
+import com.tiooooo.fintrack.component.theme.textMedium10
+import com.tiooooo.fintrack.component.theme.textMedium12
+import com.tiooooo.fintrack.component.theme.textMedium14
+import org.jetbrains.compose.resources.DrawableResource
+import org.jetbrains.compose.resources.painterResource
+
+@Composable
+fun WalletCardItem(
+    modifier: Modifier = Modifier,
+    walletItem: WalletItem,
+) {
+    Card(
+        modifier = modifier,
+        shape = RoundedCornerShape(SMALL_PADDING)
+    ) {
+        Column(
+            modifier = Modifier.padding(MEDIUM_PADDING)
+        ) {
+            ElevatedCard(
+                modifier = Modifier
+                    .size(HUGE_PADDING),
+            ) {
+                Icon(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(SMALL_PADDING),
+                    painter = painterResource(walletItem.image),
+                    contentDescription = null,
+                )
+            }
+            Text(
+                modifier = Modifier
+                    .padding(top = SMALL_PADDING)
+                    .fillMaxWidth(),
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis,
+                text = walletItem.name,
+                style = textMedium12().copy(
+                    fontWeight = FontWeight.SemiBold,
+                )
+            )
+            Text(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis,
+                text = "Di-update 12 Mar 18.30",
+                style = textMedium10().copy(fontWeight = FontWeight.Light)
+            )
+            Text(
+                modifier = Modifier
+                    .padding(top = MEDIUM_PADDING, bottom = MEDIUM_PADDING)
+                    .fillMaxWidth(),
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis,
+                text = walletItem.amount,
+                style = textMedium14().copy(
+                    fontWeight = FontWeight.Bold,
+                )
+            )
+        }
+
+    }
+}
+
+@Composable
+fun WalletCardAddItem(
+    modifier: Modifier = Modifier,
+) {
+    Card(
+        modifier = modifier,
+        shape = RoundedCornerShape(SMALL_PADDING)
+    ) {
+        Column(
+            modifier = Modifier.padding(MEDIUM_PADDING),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+        ) {
+            ElevatedCard(
+                modifier = Modifier
+                    .size(72.dp)
+                    .padding(SMALL_PADDING)
+                    .clip(RoundedCornerShape(36.dp)),
+                shape = CircleShape,
+            ) {
+                Icon(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(LARGE_PADDING),
+                    imageVector = Icons.Default.Add,
+                    contentDescription = null,
+                    tint = MaterialTheme.colorScheme.onSurface
+                )
+            }
+
+            Text(
+                modifier = Modifier
+                    .padding(top = EXTRA_SMALL_PADDING, bottom = EXTRA_LARGE_PADDING)
+                    .fillMaxWidth(),
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis,
+                textAlign = TextAlign.Center,
+                text = "Tambah",
+                style = textMedium14()
+            )
+        }
+    }
+}
+
+
+data class WalletItem(
+    val id: Int,
+    val name: String,
+    val amount: String,
+    val amountDouble : Double,
+    val color: Color,
+    val image: DrawableResource,
+)
