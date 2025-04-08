006e70274f53f3156408d03f5a1351b468862b3b
tiofani <tiofani03@gmail.com>
2025-04-08 21:01:54 +0700

    fix: adjust code

--- composeApp/src/commonMain/kotlin/com/tiooooo/fintrack/data/wallet/impl/DummyWalletRepository.kt
+++ composeApp/src/commonMain/kotlin/com/tiooooo/fintrack/data/wallet/impl/DummyWalletRepository.kt
@@ -34,19 +34,29 @@ class DummyWalletRepository : WalletRepository {
             listOf("Makan", "Transportasi", "Lainnya", "Transfer", "Gaji", "Investasi")
         val walletNames = listOf("BCA", "Jago", "Dompet", "Tabungan")
         val dates = listOf(
-            "23 Mar 2025", "24 Mar 2025", "25 Mar 2025, 26 Mar 2025, 27 Mar 2025", "28 Mar 2025", "29 Mar 2025",
-            "30 Mar 2025", "31 Mar 2025", "1 Apr 2025", "2 Apr 2025", "3 Apr 2025",
+            "23 Mar 2025",
+            "24 Mar 2025",
+            "25 Mar 2025",
+            "26 Mar 2025",
+            "27 Mar 2025",
+            "28 Mar 2025",
+            "29 Mar 2025",
+            "30 Mar 2025",
+            "31 Mar 2025",
+            "1 Apr 2025",
+            "2 Apr 2025",
+            "3 Apr 2025",
         )
 
 
         for (i in 1..numItems) {
             val transactionItem = TransactionItem(
-                id = random.nextInt(100000) + 1, // Random ID between 1 and 1000
+                id = random.nextInt(1000) + 1, // Random ID between 1 and 1000
                 date = dates[random.nextInt(dates.size)],
                 purpose = purposes[random.nextInt(purposes.size)],
                 walletName = walletNames[random.nextInt(walletNames.size)],
                 type = random.nextInt(2),
-                amount = random.nextInt(10000000) + 10
+                amount = random.nextInt(1000) + 10
             )
 
             transactionItems.add(transactionItem)
