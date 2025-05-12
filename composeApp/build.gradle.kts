499c96a8a31023c5215d43430bfbf770c80628b7
Algir <algirmhs@gmail.com>
2025-05-12 15:31:49 +0700

    update google sign in ios

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -45,7 +45,7 @@ kotlin {
             implementation(libs.androidx.lifecycle.viewmodel)
             implementation(libs.androidx.lifecycle.runtime.compose)
             implementation(project(":core:component"))
-            api(project(":core:data"))
+            implementation(project(":core:data"))
         }
     }
 }
--- core/data/src/iosMain/kotlin/com/tiooooo/fintrack/data/utils/GoogleAuthHelper.kt
+++ core/data/src/iosMain/kotlin/com/tiooooo/fintrack/data/utils/GoogleAuthHelper.kt
@@ -1,7 +1,9 @@
 package com.tiooooo.fintrack.data.utils
 
+import cocoapods.FirebaseAuth.FIRAuth
 import cocoapods.GoogleSignIn.GIDSignIn
 import com.tiooooo.fintrack.data.model.auth.AccountInfo
+import dev.gitlive.firebase.auth.GoogleAuthProvider
 import kotlinx.cinterop.ExperimentalForeignApi
 import platform.UIKit.UIApplication
 import kotlin.coroutines.resume
@@ -9,30 +11,43 @@ import kotlin.coroutines.suspendCoroutine
 
 actual class GoogleAuthHelper {
     @OptIn(ExperimentalForeignApi::class)
-    actual suspend fun signIn(): AccountInfo? =
-        suspendCoroutine { continutation ->
+    actual suspend fun signIn(): AccountInfo? = suspendCoroutine { continuation ->
+        try {
             val rootViewController = UIApplication.sharedApplication.keyWindow?.rootViewController
-
             if (rootViewController == null) {
-                continutation.resume(null)
+                continuation.resume(null)
             } else {
-                GIDSignIn.sharedInstance
-                    .signInWithPresentingViewController(rootViewController) { gidSignInResult, nsError ->
-                        nsError?.let { println("Error While signing: $nsError") }
-                        val idToken = gidSignInResult?.user?.idToken?.tokenString
-                        val profile = gidSignInResult?.user?.profile
-                        if (idToken != null) {
-                            val googleUser =
-                                AccountInfo(
-                                    token = idToken,
-                                    displayName = profile?.name ?: "",
-                                    profileImageUrl = profile?.imageURLWithDimension(320u)?.absoluteString,
-                                )
-                            continutation.resume(googleUser)
-                        } else {
-                            continutation.resume(null)
-                        }
+                GIDSignIn.sharedInstance.signInWithPresentingViewController(rootViewController) { gidSignInResult, nsError ->
+                    nsError?.let { println("Error While signing: $nsError") }
+                    val idToken = gidSignInResult?.user?.idToken?.tokenString
+                    val accessToken = gidSignInResult?.user?.accessToken?.tokenString.orEmpty()
+                    val profile = gidSignInResult?.user?.profile
+
+                    if (!idToken.isNullOrEmpty()) {
+                        FIRAuth.auth().signInWithCredential(
+                            credential = GoogleAuthProvider.credential(idToken, accessToken).ios,
+                            completion = { result, error ->
+                                if (error != null) {
+                                    println("Firebase sign in failed: $error")
+                                    continuation.resume(null)
+                                } else {
+                                    val user = result?.user
+                                    val googleUser = AccountInfo(
+                                        token = idToken,
+                                        displayName = profile?.name ?: user?.displayName ?: "",
+                                        profileImageUrl = profile?.imageURLWithDimension(320u)?.absoluteString,
+                                    )
+                                    continuation.resume(googleUser)
+                                }
+                            }
+                        )
+                    } else {
+                        continuation.resume(null)
                     }
+                }
             }
+        } catch (e: Exception) {
+            continuation.resume(null)
         }
+    }
 }
